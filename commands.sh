eec4caee8b4c5549cc9f1a8c5c87e75124482836
Boris Rose <boris.alexandre.rose@gmail.com>
2024-06-25 13:40:02 +0200

    feat/commands.sh

--- commands.sh
+++ commands.sh
@@ -9,5 +9,7 @@ install_create_git_repo_and_link_to_remote(){
 
 send_data_to_remote_repo(){
     git status #connaître l'état actuel de votre dépôit local
-    git add . #indexe toutes les modifications actuelles  
+    git add . #indexe toutes les modifications actuelles 
+    git commit -m"feat/qqch" #fix/qqch 
+    git push origin master # envoie la dernière sauvegarder sur le dépôt distant qui s'appelle origin et plus particulièrement sur sa branche master
 }
