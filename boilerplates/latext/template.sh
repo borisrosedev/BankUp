e09b812f8b2fcd14636120a6425ac26d09a21009
Boris Rose <boris.alexandre.rose@gmail.com>
2024-07-04 14:53:03 +0200

    feat/wf/test

---
+++ boilerplates/latext/template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo """
+\documentclass[a4paper, 12pt]{report}
+ % Préambule
+\usepackage{lmodern}
+\usepackage[french]{babel}
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\begin{document}
+\end{document}
+"""
